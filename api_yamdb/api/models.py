ef5e5ffb5125bf1faba8db306f85a5d20462fec4
K1N88 <m-end-eleev@yandex.ru>
2022-10-10 21:33:03 +0300

    1 commit

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
