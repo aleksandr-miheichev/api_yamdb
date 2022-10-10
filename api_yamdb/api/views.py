f029d0058400596a5db7da168355a6d3417b93b9
K1N88 <105746796+K1N88@users.noreply.github.com>
2022-10-10 22:13:09 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic.edit import CreateView
+
+from .models import CustomUser
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('v1/auth/token/')
-    template_name = ''
+class AuthorCreateView(CreateView):
+    model = CustomUser
+    fields = ['email', 'username']
