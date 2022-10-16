a3b9ea56a5be6e92b27bf09565f6ad3aabcbe979
K1N88 <105746796+K1N88@users.noreply.github.com>
2022-10-16 11:51:46 +0300

    Update urls.py
    add url to users

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (api_signup, api_token, CategoryViewSet, GenreViewSet,
-                       TitleViewSet)
+from api.views import (api_signup, api_token, AdminViewSet, api_me, api_users,
+                       CategoryViewSet, GenreViewSet, TitleViewSet)
 
 router_api_v1 = DefaultRouter()
 
@@ -13,5 +13,12 @@ router_api_v1.register('titles', TitleViewSet, basename='titles')
 urlpatterns = [
     path('v1/auth/signup/', api_signup, name='signup'),
     path('v1/auth/token/', api_token, name='token'),
+    path(
+        'v1/users/',
+        AdminViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='admin_signup'
+    ),
+    path('v1/users/me/', api_me, name='me'),
+    path('v1/users/<str:username>/', api_users, name='users'),
     path('v1/', include(router_api_v1.urls)),
 ]
