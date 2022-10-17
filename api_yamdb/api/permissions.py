93be86f87d03814e0c791398ac5a56f1b51b4130
Николай Тищенко <nikolay_tishenko90@mail.ru>
2022-10-17 22:33:06 +0300

    отзывы и комменты

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -13,3 +13,19 @@ class IsAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated
                 and request.user.role == 'admin' or request.user.is_superuser)
+
+
+class IfAdminModeratorAuthorPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.role == 'moderator'
+            or request.user.role == 'admin'
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,15 +13,17 @@ from rest_framework.permissions import AllowAny
 
 from api.filters import TitleFilter
 from api.mixins import CreateDestroyListViewSet
-from api.permissions import IsAdminOnly, IsStaffOrReadOnly
+from api.permissions import IsAdminOnly, IsStaffOrReadOnly, IfAdminModeratorAuthorPermission
 from api.serializers import (
     CategorySerializer,
     GenreSerializer,
     GetTitleSerializer,
-    TitleSerializer
+    TitleSerializer,
+    ReviewSerializer,
+    CommentSerializer
 )
 from api_yamdb.settings import DEFAULT_FROM_EMAIL
-from reviews.models import Category, CustomUser, Genre, Title
+from reviews.models import Category, CustomUser, Genre, Title, Comment, Review
 from reviews.serializers import (
     AdminSerializer,
     MeSerializer,
@@ -179,3 +181,37 @@ class TitleViewSet(viewsets.ModelViewSet):
         if self.action in ('retrieve', 'list'):
             return GetTitleSerializer
         return TitleSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (IfAdminModeratorAuthorPermission,)
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Title,
+            pk=self.kwargs.get('title_id'))
+        return review.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(
+            Title,
+            pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IfAdminModeratorAuthorPermission,)
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Review,
+            pk=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(
+            Review,
+            pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
