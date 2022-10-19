856de3930d109884d800861dd7c6ec709b524cb2
Николай Тищенко <nikolay_tishenko90@mail.ru>
2022-10-19 22:18:31 +0300

    правки в сериализаторе и моделях для review и comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -170,82 +170,65 @@ class GenreTitle(models.Model):
         return f'Жанр у произведения "{self.title}": {self.genre}'
 
 
-class Review(models.Model):
+class CommonInfo(models.Model):
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s',
+        verbose_name='Автор',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата публикации'
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Review(CommonInfo):
     title = models.ForeignKey(
         Title,
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='Произведение'
     )
-    text = models.TextField(
-        verbose_name='Текст отзыва',
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='reviews',
-        verbose_name='Автор',
-    )
     score = models.IntegerField(
         default=1,
-        validators=[
-            MinValueValidator(
-                1,
-                message='Минимальное значение должно быть более 1!'
-            ),
-            MaxValueValidator(
-                10,
-                message='Максимальное значение должно быть менее 10!'
-            )
-        ]
-    )
-    pub_date = models.DateTimeField(
-        auto_now_add=True,
-        db_index=True,
-        verbose_name='Дата отзыва'
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ),
+        error_messages={'invalid': 'Please rate from 1 to 10'}
     )
 
-    class Meta:
+    class Meta(CommonInfo.Meta):
         constraints = [
             models.UniqueConstraint(
                 fields=['author', 'title'],
                 name='unique_review'
             )
         ]
-        ordering = ('-pub_date',)
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
 
-    def __str__(self):
-        return self.text[:15]
 
-
-class Comment(models.Model):
+class Comment(CommonInfo):
     review = models.ForeignKey(
         Review,
         on_delete=models.CASCADE,
         related_name='comments',
         verbose_name='Отзыв'
     )
-    text = models.TextField(
-        verbose_name='Текст комментария',
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор комментария'
-    )
-    pub_date = models.DateTimeField(
-        auto_now_add=True,
-        db_index=True,
-        verbose_name='Дата публикации комментария'
-    )
 
-    class Meta:
+    class Meta(CommonInfo.Meta):
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
-        ordering = ('-pub_date',)
-
-    def __str__(self):
-        return self.text[:15]
