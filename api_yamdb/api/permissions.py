c120748936427bcc02628a67cd0e2aae428df73a
NikolayTishenko <105671936+NikolayTishenko@users.noreply.github.com>
2022-10-21 21:53:07 +0300

    Update permissions.py
    изменение в IfAdminModeratorAuthorPermission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -31,6 +31,6 @@ class IfAdminModeratorAuthorPermission(permissions.BasePermission):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
-            or request.user.role == 'moderator'
-            or request.user.role == 'admin'
+            or request.user.role == CustomUser.MODERATOR
+            or request.user.role == CustomUser.ADMIN
         )
