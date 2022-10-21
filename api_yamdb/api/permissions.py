2ba8c0ff2e5052c4aa8802f433850a16e7d8e673
NikolayTishenko <105671936+NikolayTishenko@users.noreply.github.com>
2022-10-21 21:55:31 +0300

    Update permissions.py
    изменение в IsAdminOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -16,7 +16,8 @@ class IsAdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and request.user.role == 'admin' or request.user.is_superuser
+            and request.user.role == CustomUser.ADMIN
+            or request.user.is_superuser
         )
 
 
