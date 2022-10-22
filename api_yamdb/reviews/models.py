dc8ff599eec791fb874c5b1059624a74ae93cb78
K1N88 <105746796+K1N88@users.noreply.github.com>
2022-10-22 18:08:59 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.core.validators import EmailValidator
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 from reviews.validators import validate_year, validate_username
 
